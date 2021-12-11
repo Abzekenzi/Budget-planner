06347f897c32efe1f9bfd52b30bb9b80b895fe6c
Abzekenzi <abzala@icloud.com>
2021-12-11 23:14:46 +0300

    Планировщик бюджета
    Описание
        Подправил критерий выбора ветвления и добавил частично предложение меню без выбора
    Автор
        Абзал

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import unittest
-
-class TestMethods(unittest.TestCase):
-    def test_validate(self):
-        self.assertEqual(validate(50), 0)
-
-if __name__ == '__main__':
-    unittest.main()
-
 #Function of getting input
 def get_input():
     print("Введите сумму денег")
@@ -27,22 +18,68 @@ def readfile(filename):
 
 #Function of choosing the method to apply
 def validate(sum):
+    main = readfile('main')
+    soups = readfile('soups')
+    min_price_main = float(min(main.values()))
+    min_price_soups = float(min(soups.values()))
+    max_price_main = float(max(main.values()))
+    max_price_soups = float(max(soups.values()))
+
+    if sum < min_price_main + min_price_soups:
+        return 0
+    if (sum >= min_price_main + min_price_soups) and (sum < max_price_main + max_price_soups):
+        return 1
+    if sum >= max_price_main + min_price_soups:
+        return 2
+
+def get_minimal_meal(data):
+    min_key = ""
+    min = float(list(data.values())[0])
+    for i in data.keys():
+        curr = float(data[i])
+        if curr < min:
+            print(i)
+            min_key = i
+            min = curr
+
+    return [min_key, min]
+
+
+def start_providing(mode):
+    if mode != 1 and mode != 2:
+        return -1
+
     coffee = readfile('coffee')
     drinks = readfile('drinks')
     garnish = readfile('garnish')
     main = readfile('main')
     soups = readfile('soups')
     tea = readfile('tea')
-    min_price = float(min(min(main.values()), min(soups.values())))
-    max_price = float(max(max(main.values()), max(soups.values())))
 
-    if sum < min_price:
-        return 0
-    #if sum >= min_price and
+    if mode == 1:
+        resulting_menu = {}
+        tmp_meal = get_minimal_meal(main)
+        resulting_menu[tmp_meal[0]] = tmp_meal[1]
+        tmp_meal = get_minimal_meal(soups)
+        resulting_menu[tmp_meal[0]] = tmp_meal[1]
+        return resulting_menu
 
 #Start of the program
 print("Здравтсвуйте! Пожалуйста, ответьте на следующие вопросы")
 input_res = get_input()
 sum_per_consuption = input_res[0] / (input_res[1] * 2)
-if validate(sum_per_consuption) == 0:
+val = validate(sum_per_consuption)
+if val == 0:
     print("К сожалению, мы не можем Вам предложить план питания: недостаточно денег")
+    exit()
+elif val == 1:
+    print("К сумме, указанной Вами, предлагаем следующий вариант")
+elif val == 2:
+    print("Выберите из предложенных вариантов")
+
+menu = start_providing(val)
+if menu == -1:
+    print("Bad option")
+    exit()
+print(menu)
+
